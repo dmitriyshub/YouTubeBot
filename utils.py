6131ecea357a4a39d0158d3be1e94cfef0136f8c
dmitriyshub <d.shtranvasser@gmail.com>
2022-11-19 14:07:26 +0200

    add calc backlog per instance function to utils

--- utils.py
+++ utils.py
@@ -12,3 +12,41 @@ def search_download_youtube_video(video_name, num_results=1):
         videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
 
     return [ydl.prepare_filename(video) for video in videos]
+
+def calc_backlog_per_instance(sqs_queue_client, asg_client, asg_group_name):
+    while True:
+        msgs_in_queue = int(sqs_queue_client.attributes.get('ApproximateNumberOfMessages'))
+        asg_size = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_group_name])['AutoScalingGroups'][0]['DesiredCapacity']
+
+        if msgs_in_queue == 0:
+            backlog_per_instance = 0
+        elif asg_size == 0:
+            backlog_per_instance = 99
+        else:
+            backlog_per_instance = msgs_in_queue / asg_size
+
+        logger.info(f'backlog per instance: {backlog_per_instance},msg:{msgs_in_queue},workers:{asg_size}')
+        # TODO send the backlog_per_instance metric to cloudwatch
+
+        # Create CloudWatch client
+        cloudwatch = boto3.client('cloudwatch')
+
+        # Put custom metrics
+        cloudwatch.put_metric_data(
+            MetricData=[
+                {
+                    'MetricName': 'backlog_per_instance',
+                    'Dimensions': [
+                        {
+                            'Name': 'count_backlog',
+                            'Value': 'value_backlog'
+                        },
+                    ],
+                    'Unit': 'None',
+                    'Value': backlog_per_instance
+                },
+            ],
+            Namespace='dmitriyshub/metrics'
+        )
+
+        time.sleep(5)
